38e20fbb1874ec0eba710abcc6a571f7bbc23296
Sajal <sajal89304@gmail.com>
2023-11-10 00:03:29 +0530

    complete vendor crud complete

--- apps/bills/urls.py
+++ apps/bills/urls.py
@@ -9,6 +9,10 @@ urlpatterns = [
     path("edit-bill/", views.EditBill.as_view(), name="edit-bill"),
     path("vendors/", views.VendorsView.as_view(), name="vendors"),
     path("add-vendors/", views.VendorCreate.as_view(), name="add-vendors"),
-    path("ediit-vendors/", views.VendorEdit.as_view(), name="edit-vendors"),
+    path("ediit-vendors/<int:id>/", views.VendorEdit.as_view(), name="edit-vendors"),
+    path("delete-vendors/<int:id>/", views.vendor_delete, name="delete-vendors"),
+
+
+    
 
 ]
--- apps/bills/views.py
+++ apps/bills/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from . models import *
+
 # Create your views here.
 
 from apps.invoice.models import Product_Service ,Tax
@@ -57,8 +58,12 @@ class EditBill(View):
 
 class VendorsView(View):
     def get(self, request):
+        vendor = Vendor.objects.all()
+        context = {
+            "vendor":vendor
+        }
         
-        return render(request, "vendors.html",)
+        return render(request, "vendors.html",context)
 
 
 
@@ -67,17 +72,55 @@ class VendorCreate(View):
         currency = Currency.objects.all()
         context = {
             "currency":currency
-
         }
-        
         return render(request, "vendors_add.html",context)
     
     def post(self,request):
-        pass
-      
+        name = request.POST.get("name")
+        email = request.POST.get("email")
+        currency = request.POST.get("currency")
+        
+        address1 = request.POST.get("address1")
+        address2 = request.POST.get("address2")
+        city = request.POST.get("city")
+        postal = request.POST.get("postal")
+        phone = request.POST.get("phone")
+        obj = Vendor.objects.create(name=name, email=email,currency_id=currency,address1=address1,address2=address2,
+                                    city=city,postal=postal,phone=phone,)
+
+        return redirect("vendors")
+        
 
 
 class VendorEdit(View):
-    def get(self, request):
-        return render(request, "vendors_edit.html",)
+    def get(self, request,id):
+        response = Vendor.objects.get(id=id)
+        currency   = Currency.objects.all()
+        context = {
+            "response" :response ,
+            "currency"    :currency
+        }
+
+        return render(request, "vendors_edit.html",context)
+    def post(self,request,id):
+        response = Vendor.objects.get(id=id)
+        response.name = request.POST.get("name")
+        response.email = request.POST.get("email")
+        response.currency_id = request.POST.get("currency")
+        response.address1 = request.POST.get("address1")
+        response.address2 = request.POST.get("address2")
+        response.city = request.POST.get("city")
+        response.postal = request.POST.get("postal")
+        response.phone = request.POST.get("phone")
+        response.save()
+        return redirect("vendors")
+    
+
+
+def vendor_delete(request,id):
+    print("This is  vill rin ")
+    obj = Vendor.objects.get(id=id)
+    obj.delete()
+    return redirect("vendors")
+
     
